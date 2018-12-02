46dddb5ce001a7cfbe8edd6a1b2f8b5b9adb5aab
Toon Borgers <toon.b@sparkcentral.com>
2018-12-02 19:27:51 +0100

    cleanp

--- kotlin/src/main/kotlin/be/borgers/aoc/day1/Day1.kt
+++ kotlin/src/main/kotlin/be/borgers/aoc/day1/Day1.kt
@@ -4,22 +4,25 @@ import be.borgers.aoc.util.getInputAsInts
 
 fun main(args: Array<String>) {
     part1()
+    part2()
+}
 
+private fun part1() {
+    println(getInputAsInts(1)
+            .reduce { sum, element -> sum + element })
+}
+
+private fun part2() {
     val history = mutableListOf(0)
     val inputs = getInputAsInts(1)
     var index = 0
 
     while (true) {
         val curr = (history.last() + inputs[index++ % inputs.size])
-        if(history.contains(curr)){
+        if (history.contains(curr)) {
             println(curr)
             return
         }
         history.add(curr)
     }
-}
-
-private fun part1() {
-    println(getInputAsInts(1)
-            .reduce { sum, element -> sum + element })
 }
